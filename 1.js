b2fac603b4f5a9b69b1ae48ea4796113852a6c56
Ben Walker <bwalke05@gmail.com>
2019-03-20 13:19:00 -0400

    Solve problem 1

--- 1.js
+++ 1.js
@@ -3,3 +3,23 @@
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass? */
+
+const addToK = (list, k) => {
+  const tracker = [];
+  let check = false;
+
+  list.forEach((i) => {
+    if (tracker.includes(i)) check = true;
+    tracker.push(k - i);
+  });
+
+  return check;
+};
+
+addToK([10, 15, 3, 7], 17); // true
+addToK([], 17); // false
+addToK([17], 17); // false
+addToK([17, 0], 17); // true
+addToK([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 17); // true
+addToK([0, 0], 0); // true
+addToK([15, 2, 3, 0, -1, 2, 3, -5], -6); // true
