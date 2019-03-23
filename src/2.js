de2775c986547f0762e63a2e9e683207ab7a19c1
Ben Walker <bwalke05@gmail.com>
2019-03-23 16:31:33 -0400

    Match new documentation style; minor refactoring

--- src/2.js
+++ src/2.js
@@ -6,17 +6,21 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be
 
 Follow-up: what if you can't use division? */
 
+/* Implementation */
 const prodArray = (ogArray) => {
   const prods = [];
 
-  ogArray.forEach((e, i) => {
+  ogArray.forEach((_e, i) => {
+    // build a duplicate array but omit the current value, calculate the product of the new array
     const arraySansIndex = ogArray.slice(0, i).concat(ogArray.slice(i + 1));
-    prods.push(arraySansIndex.reduce((a, b) => a * b));
+    const product = arraySansIndex.reduce((a, b) => a * b); // division is not used
+    prods.push(product);
   });
 
   return prods;
 };
 
+/* Testing */
 console.assert(JSON.stringify(prodArray([1, 2, 3, 4, 5]))
   === JSON.stringify([120, 60, 40, 30, 24]));
 console.assert(JSON.stringify(prodArray([3, 2, 1]))
