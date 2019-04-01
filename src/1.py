68ca901f76795c14936b3db45d7490a7ca08b409
Ben Walker <bwalke05@gmail.com>
2019-04-01 13:25:17 -0400

    Improve solution format

--- src/1.py
+++ src/1.py
@@ -7,8 +7,6 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
 '''
 
-# Implementation
-
 
 def add_to_k(numbers, k):
     tracker = []
